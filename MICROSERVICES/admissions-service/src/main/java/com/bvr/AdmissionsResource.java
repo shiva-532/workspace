81d59c076a18e724da7471a1a2476581336ff5c3
vishymails <vishymails@gmail.com>
2021-12-14 15:08:35 +0530

    added

--- MICROSERVICES/admissions-service/src/main/java/com/bvr/AdmissionsResource.java
+++ MICROSERVICES/admissions-service/src/main/java/com/bvr/AdmissionsResource.java
@@ -19,14 +19,14 @@ public class AdmissionsResource {
 	
 	@RequestMapping("/physicians")
 	public EmployeeList getPhysicians() {
-		EmployeeList physicians = restTemplate.getForObject("http://localhost:8082/hr/employees", EmployeeList.class);
+		EmployeeList physicians = restTemplate.getForObject("http://hr-service/hr/employees", EmployeeList.class);
 		return physicians;
 	}
 	
 	@RequestMapping("/currentDiseases")
 	public DiseaseList getDiseases() {
 		
-		DiseaseList diseases =  restTemplate.getForObject("http://localhost:8083/pathology/diseases", DiseaseList.class);
+		DiseaseList diseases =  restTemplate.getForObject("http://pathology-service/pathology/diseases", DiseaseList.class);
 		return diseases;
 	}
 }
