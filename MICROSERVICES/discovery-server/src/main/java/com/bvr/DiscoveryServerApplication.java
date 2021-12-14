24e617f9bc0fef4c967068367e913f3180c5a3a7
vishymails <vishymails@gmail.com>
2021-12-14 14:24:29 +0530

    added

--- MICROSERVICES/discovery-server/src/main/java/com/bvr/DiscoveryServerApplication.java
+++ MICROSERVICES/discovery-server/src/main/java/com/bvr/DiscoveryServerApplication.java
@@ -2,14 +2,14 @@ package com.bvr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class PathologyServiceApplication {
+@EnableEurekaServer
+public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PathologyServiceApplication.class, args);
+		SpringApplication.run(DiscoveryServerApplication.class, args);
 	}
 
 }
